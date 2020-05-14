ce2ba3fa0a4eb2c8ccfeab9b269af8cd591ceb3c
MuhammadSalmanMehmood <connect_salman@live.com>
2020-05-14 06:39:13 +0500

    Dice App Flutter Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 1;
+  void changer() {
+    setState(
+      () {
+        leftdicenumber = Random().nextInt(6) + 1;
+        rightdicenumber = Random().nextInt(6) + 1;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changer();
+              },
+              child: Image.asset('images/dice$leftdicenumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changer();
+              },
+              child: Image.asset('images/dice$rightdicenumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
